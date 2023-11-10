7f2eb9361418af15bdf9812708effb89a0b1f219
dilshodbekkhamroev98@gmail.com <dilshodbekkhamroev98@gmail.com>
2023-11-11 01:11:10 +0500

    EXPOSER: Groups Add(WIP)

---
+++ xChanger.MVC/Controllers/GroupsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using xChanger.MVC.Services.Orchestrations;
+
+namespace xChanger.MVC.Controllers
+{
+    public class GroupsController : Controller
+    {
+        private readonly IOrchestrationService orchestrationService;
+        public GroupsController(IOrchestrationService orchestrationService)
+        {
+            this.orchestrationService = orchestrationService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}
